0be983a3be4d4825b77d222506f675c64c1a11b7
Rocky Haque <rockyhaque99@gmail.com>
2024-12-31 08:53:24 +0600

    error handling 1.0

--- src/middlewares/globalErrorHandler.ts
+++ src/middlewares/globalErrorHandler.ts
@@ -11,7 +11,7 @@ type TErrorResponse = {
 }
 
 export const globalErrorHandler = (
-  err: TErrorResponse,
+  err: any,
   req: Request,
   res: Response,
   _next: NextFunction
@@ -22,7 +22,42 @@ export const globalErrorHandler = (
       message: err.message,
       error: err,
     })
-  } else if (err instanceof Error) {
+  }
+
+  else if(err instanceof mongoose.Error.ValidationError) {
+    res.status(StatusCodes.BAD_REQUEST).json({
+      sucess: false,
+      message: err.message,
+      error: err,
+    })
+  }
+  
+
+  //! Not Working Properly - handle duplicate key error
+  else if (err.code && err.code === 11000) {
+    res.status(StatusCodes.BAD_REQUEST).json({
+      success: false,
+      message: err.errResponse.errmsg,
+      error: err,
+    })
+  }
+
+  //! Wroking - Alterantive way to handle duplicate key error
+
+  // else if (err.code && err.code === 11000) {
+  //   // Handling duplicate key error
+  //   const keyValue = err.keyValue || err.error?.keyValue
+  //   const duplicateField = Object.keys(keyValue || {}).join(', ')
+  //   const duplicateValue = Object.values(keyValue || {}).join(', ')
+  //   res.status(StatusCodes.BAD_REQUEST).json({
+  //     success: false,
+  //     message: `Duplicate value detected for field: '${duplicateField}' with value: '${duplicateValue}'. Please use a unique value.`,
+  //     error: err,
+  //   })
+  // } 
+
+
+  else if (err instanceof Error) {
     res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
       success: false,
       name: err.name,
@@ -30,4 +65,6 @@ export const globalErrorHandler = (
       error: err,
     })
   }
+
+
 }
